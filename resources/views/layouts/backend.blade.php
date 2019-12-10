324f76a8b6fdf98c8859f4ad38c305e91583c49a
ashish.kumar <ashish.kumar@netscapelabs.com>
2019-12-09 19:00:41 -0800

    push to live

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -32,17 +32,17 @@
 
 <!--datatable starts-->
 
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
-        <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
-        <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+<link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
-        <!--datatable ends-->
+<!--datatable ends-->
 </head>
 <body>
     <div class="page-container">
@@ -113,11 +113,11 @@
                     <li><a href="{{ url('admin/users')}}"><i class="fa fa-users"></i> <span>Users</span></a></li>
                     <!--<li><a href="{{ url('admin/roles')}}"><i class="fa fa-rocket"></i> <span>Roles</span></a></li>-->
                     <!--<li><a href="{{ url('admin/game')}}"><i class="fa fa-rocket"></i> <span>Game</span></a></li>-->
-                <li><a href="{{ url('admin/competition')}}"><i class="fa fa-rocket"></i> <span>Competition</span></a></li>
-                     <li><a href="{{ url('admin/news')}}"><i class="fa fa-rocket"></i> <span>News</span></a></li>
-     <li><a href="{{ url('admin/competition-categories')}}"><i class="fa fa-rocket"></i> <span>Competition Category</span></a></li>
-                    <!--<li><a href="{{ url('admin/permissions')}}"><i class="fa fa-rocket"></i> <span>Permissions</span></a></li>-->
-        <!--<li><a href="{{ url('admin/generator')}}"><i class="fa fa-bars"></i> <span>Generator</span></a></li>-->
+                    <li><a href="{{ url('admin/competition')}}"><i class="fa fa-rocket"></i> <span>Competition</span></a></li>
+                    <li><a href="{{ url('admin/news')}}"><i class="fa fa-rocket"></i> <span>News</span></a></li>
+                    <li><a href="{{ url('admin/competition-categories')}}"><i class="fa fa-rocket"></i> <span>Competition Category</span></a></li>
+                                   <!--<li><a href="{{ url('admin/permissions')}}"><i class="fa fa-rocket"></i> <span>Permissions</span></a></li>-->
+                       <!--<li><a href="{{ url('admin/generator')}}"><i class="fa fa-bars"></i> <span>Generator</span></a></li>-->
 
                 </ul>
             </div>
@@ -142,11 +142,21 @@
 
             toggle = !toggle;
         });
+//        $("#menu li a").each(function () {
+//            var basepath = window.location.pathname.split('/')[4];
+//            if ($(this).attr('href').split('/')[6] == basepath) {
+//                $(this).parent().addClass('active');
+//            }
+//        });
+
         $("#menu li a").each(function () {
-            var basepath =  window.location.pathname.split('/')[4];
-        if($(this).attr('href').split('/')[6] == basepath){
-               $(this).parent().addClass('active');
-           }
+            $(this).parent().removeClass('active');
+            var basepath = window.location.pathname.split('/')[3];
+            if ($(this).attr('href').split('/')[5] == basepath) {
+//                console.log(basepath);
+//                console.log($(this).attr('href').split('/')[5]);
+                $(this).parent().addClass('active');
+            }
         });
     </script>
     <!--js -->
