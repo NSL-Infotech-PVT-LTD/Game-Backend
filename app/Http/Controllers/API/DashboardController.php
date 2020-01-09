6cc202b2802b3f8c8deb65b96d75bbcdc07a980c
ergauravsethi376 <gaurav@netscapelabs.com>
2020-01-09 16:47:50 +0530

    push to live

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends ApiController {
         try {
 
 //            dd(\App\Competition::whereDate('date', '=', \Carbon\Carbon::now())->get()->pluck('id')->toArray());
-            dd(\App\CompetitionUser::whereIn('competition_id',\App\Competition::whereDate('date', '=', \Carbon\Carbon::now())->get()->pluck('id')->toArray())->get()->pluck('player_id')->toArray());
+//            dd(\App\CompetitionUser::whereIn('competition_id',\App\Competition::whereDate('date', '=', \Carbon\Carbon::now())->get()->pluck('id')->toArray())->get()->pluck('player_id')->toArray());
 //            \App\Http\Controllers\API\ApiController::pushNotificationsMultipleUsers(['title' => ' Added', 'body' => 'custom'], ['63'], ['target_id' => '222', 'target_type' => 'testing'],'FCM');
             $limit = 50;
             return parent::success(['game' => \App\Game::select('id', 'name', 'image')->limit($limit)->get(), 'competition' => \App\Competition::select('id', 'name', 'image', 'description', 'date', 'fee', 'prize_details', 'game_id', 'competition_category_id', 'state')->whereDate('date', '=', \Carbon\Carbon::now())->limit($limit)->with(['game', 'category'])->get(), 'previous_winner' => \App\Previouswinner::select('id', 'title', 'description', 'image', 'state')->limit($limit)->get(), 'news' => \App\News::select('id', 'title', 'description', 'image')->limit($limit)->get()]);
