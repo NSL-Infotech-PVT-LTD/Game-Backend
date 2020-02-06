979e5bc131f4715bcb05ffe17f2f28837babe0d1
ergauravsethi376 <gaurav@netscapelabs.com>
2020-02-06 20:06:02 +0530

    push to live

--- resources/views/admin/metas/edit.blade.php
+++ resources/views/admin/metas/edit.blade.php
@@ -7,7 +7,7 @@
 
         <div class="col-md-9">
             <div class="card">
-                <div class="card-header">Edit Meta {{ ucfirst(str_replace('_',' ',$meta->meta_key)) }}</div>
+                <div class="card-header">Edit {{ ucfirst(str_replace('_',' ',$meta->meta_key)) }}</div>
                 <div class="card-body">
                     <a href="{{ url('/admin') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
                     <br />
