5d4946de287374f0558cc6c0208bb8b6e98bde5f
ashish.kumar <ashish.kumar@netscapelabs.com>
2020-02-12 19:34:02 -0800

    changes in cron job

--- app/Console/Commands/NotifyCompetitionBefore.php
+++ app/Console/Commands/NotifyCompetitionBefore.php
@@ -19,8 +19,8 @@ class NotifyCompetitionBefore extends Command {
      * @var string
      */
     protected $description = 'Send a Push notification to all enrolled users on before of game';
-    protected $title = 'Game is Going to start';
-    protected $body = 'Game is Going to start';
+    protected $title = 'The competition is about to go LIVE';
+    protected $body = '2 hours to go until the competition is LIVE ⏰ Get some practice to have the best chance of winning.';
 
     /**
      * Create a new command instance.
