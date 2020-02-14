b4feb95fac0eb2ed801049b57393029f8e30b0b7
ashish.kumar <ashish.kumar@netscapelabs.com>
2020-02-14 13:24:37 -0800

    change mail name

--- config/mail.php
+++ config/mail.php
@@ -49,7 +49,7 @@ return [
       |
      */
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@winchunks.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
     /*
