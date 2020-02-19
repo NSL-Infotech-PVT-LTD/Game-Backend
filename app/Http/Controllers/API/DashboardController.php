abe82a6d90eeb8d53df7268cd315b9957b67fa0d
ergauravsethi376 <gaurav@netscapelabs.com>
2020-02-19 18:54:56 +0530

    push to live

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -23,17 +23,17 @@ class DashboardController extends ApiController {
         endif;
         try {
 
-            $date = new \DateTime();
-            $date->modify('-2 hours');
-            $formatted_time = $date->format('H:i:s');
-//        dd($formatted_time);
-            $competitions = \App\Competition::whereDate('date', '=', \Carbon\Carbon::now())->where('start_time', '>', $formatted_time)->get()->pluck('id')->toArray();
-//            dd($competitions);
-            foreach ($competitions as $competition_id):
-                $userIds = \App\CompetitionUser::where('competition_id', $competition_id)->get()->pluck('player_id')->toArray();
-//                dd($userIds);
-                \App\Http\Controllers\API\ApiController::pushNotificationsMultipleUsers(['title' => $this->title, 'body' => $this->body], array_unique($userIds), ['target_id' => $competition_id, 'target_type' => 'Competition'], 'FCM');
-            endforeach;
+//            $date = new \DateTime();
+//            $date->modify('-2 hours');
+//            $formatted_time = $date->format('H:i:s');
+////        dd($formatted_time);
+//            $competitions = \App\Competition::whereDate('date', '=', \Carbon\Carbon::now())->where('start_time', '>', $formatted_time)->get()->pluck('id')->toArray();
+////            dd($competitions);
+//            foreach ($competitions as $competition_id):
+//                $userIds = \App\CompetitionUser::where('competition_id', $competition_id)->get()->pluck('player_id')->toArray();
+////                dd($userIds);
+//                \App\Http\Controllers\API\ApiController::pushNotificationsMultipleUsers(['title' => $this->title, 'body' => $this->body], array_unique($userIds), ['target_id' => $competition_id, 'target_type' => 'Competition'], 'FCM');
+//            endforeach;
             $limit = 50;
             return parent::success(['game' => \App\Game::select('id', 'name', 'image')->limit($limit)->orderBy('id', 'desc')->get(), 'competition' => \App\Competition::select('id', 'name', 'image', 'description', 'date', 'start_time', 'fee', 'prize_details', 'sequential_fee', 'game_id', 'competition_category_id', 'state')->whereDate('date', '>=', \Carbon\Carbon::now()->toDateString())->where('hot_competitions', '1')->limit($limit)->with(['game', 'category'])->orderBy('id', 'desc')->get(), 'previous_winner' => \App\Previouswinner::select('id', 'title', 'description', 'image', 'state')->limit($limit)->orderBy('id', 'desc')->get(), 'news' => \App\News::select('id', 'title', 'description', 'image')->limit($limit)->orderBy('id', 'desc')->get()]);
         } catch (\Exception $ex) {
