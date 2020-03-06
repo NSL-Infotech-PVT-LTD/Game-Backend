12ce5ce8d57193a7df697211533240c7175b1819
shivam.tyagi <shivam.tyagi@netscapelabs.com>
2020-03-06 12:22:36 +0530

    Fix bug on meta and add state in dashboard API

--- app/Http/Controllers/API/MetasController.php
+++ app/Http/Controllers/API/MetasController.php
@@ -10,19 +10,30 @@ use Validator;
 class MetasController extends ApiController {
 
     public function getMeta(Request $request) {
-        $validator = Validator::make($request->all(), [
-                    'meta_key' => 'required',
-        ]);
-        if ($validator->fails()) {
-            $errors = parent::formatValidator($validator);
-            return parent::error($errors, 200);
-        }
-        $details = MyModel::where('meta_key', $request->input('meta_key'))->first();
-        //dd($details);
-        if (count($details) > 0) {
-            return parent::success($details, $this->successStatus);
+        // $validator = Validator::make($request->all(), [
+        //             'meta_key' => 'required',
+        // ]);
+        // if ($validator->fails()) {
+        //     $errors = parent::formatValidator($validator);
+        //     return parent::error($errors, 200);
+        // }
+         $rules = ['meta_key' => 'required'];
+//        dd($request->all());
+        $validateAttributes = parent::validateAttributes($request, 'POST', $rules, array_keys($rules), false);
+        if ($validateAttributes):
+            return $validateAttributes;
+        endif;
+        try {
+        $details = MyModel::where('meta_key', $request->input('meta_key'));
+        // dd($details->get()->isEmpty());
+        if ($details->get()->isEmpty() === false) {
+            return parent::success($details->first(), $this->successStatus);
         } else {
             return parent::error('No ' . $request->input('meta_key') . ' Found', 200);
+        }
+             // return parent::success($model->paginate($perPage));
+        } catch (\Exception $ex) {
+            return parent::error($ex->getMessage());
         }
     }
 
