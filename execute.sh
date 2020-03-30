dfb563d7544bb664179cc8dee47cb280486e7889
ergauravsethi376 <gaurav@netscapelabs.com>
2020-03-30 20:33:00 +0530

    push to live

--- execute.sh
+++ execute.sh
@@ -1,6 +1,6 @@
 cd /var/www/html/win_chunks
 sudo git pull origin development-0.0.3
 #composer install
-chmod -R 0777 /var/www/html/win_chunks/storage /var/www/html/win_chunks/bootstrap  
+sudo chmod -R 0777 /var/www/html/win_chunks/storage /var/www/html/win_chunks/bootstrap  
 exit
 
